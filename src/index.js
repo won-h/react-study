add01c43c24b5b640967ab421e842694bc4bb9bc
dnjs02018 <dnjs02018@naver.com>
2022-11-08 22:08:59 +0900

    clock

--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import Clock from './chapter_04/Clock';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+setInterval(()=> {
+  root.render(
+    <React.StrictMode>
+      <Clock />
+      {/* <App /> */}
+    </React.StrictMode>
+  )
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
